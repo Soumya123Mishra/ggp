93fd0908cf9ba80de1a9157d09ba22583eada82b
Soumya mishra <113369816+Soumya123Mishra@users.noreply.github.com>
2023-09-12 13:31:26 +0530

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const myModalEl = document.querySelector('#loginModal')
+const modal = new bootstrap.Modal(myModalEl) // initialized with defaults
+
+const configObject = { keyboard: false }
+const modal1 = new bootstrap.Modal(myModalEl, configObject) // initialized with no keyboard
