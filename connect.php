61758076ca641b3f22fd6981dcf572fdc7f2b8b1
Soumya mishra <113369816+Soumya123Mishra@users.noreply.github.com>
2023-09-23 14:46:03 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,5 +5,13 @@ $address=$_POST['address'];
 $password=$_POST['password'];
 
 //
-
+$conn = new mysqli('localhost','root','','test');
+if($conn->connect_error){
+die('connection Failed :'.$conn->connect_error);
+}else{
+$stmt = $conn->prepare("insert into contsct(name,email,password,address)values(?,?,?,?<)");
+  $stmt->bind_param("ssssi",$name,$email,$password,$address);
+  $stmt->close();
+  $conn->close();
+}
 ?>
